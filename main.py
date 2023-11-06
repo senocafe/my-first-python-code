6c824754010ad55483d6616e060ea6483a975573
rizaagusdev <riza.agus.a@gmail.com>
2023-11-06 11:13:52 +0700

    Sintaksis dasar

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
-print("Hello World")
-print("My name is Baba Riza")
+# print("Hello World")
+# print("My name is Baba Riza")
+
+"""
+Semua sitaksis dasar bahasa pemrograman terdiri dari:
+1. Sekuensial   : langkah berurutan
+2. Percabangan  : langkah melompat jika kondisi terpenuhi
+3. Perulangan   : mengulang langkah yang sama berkali-kali selama/sampai kondisi terpenuhi
+"""
+# Sekuensial
+print('Ibu berkata, "Pergi ke toko"')
+print('Anak Menjawab, "Baik, apa yang harus saya lakukan ditoko?"')
+print('Ibu Menjawab, "Beli satu botol susu, jika ada telur beli 6"')
+print('Maka Anak berangkat ke toko')
+print('Dan mulai berbelanja')
+
 
