40b38151c48c034d19eb7e5525de62bc2338a3fe
rizaagusdev <riza.agus.a@gmail.com>
2023-11-07 10:07:44 +0700

    Commit Tugas Percabangan "Honey, please go to the market and buy 1 bottle of milk. If they have eggs, bring 6"

--- main.py
+++ main.py
@@ -11,7 +11,24 @@ Semua sitaksis dasar bahasa pemrograman terdiri dari:
 print('Ibu berkata, "Pergi ke toko"')
 print('Anak Menjawab, "Baik, apa yang harus saya lakukan ditoko?"')
 print('Ibu Menjawab, "Beli satu botol susu, jika ada telur beli 6"')
-print('Maka Anak berangkat ke toko')
-print('Dan mulai berbelanja')
+print('Maka Anak pergi ke toko')
+print('Anak bertanya ke toko, "Apakah susu ada?"')
 
+# Percabangan
+jumlah_botol_susu = 173
+jumlah_telur = 1587
+print(f'jumlah botol susu {jumlah_botol_susu} botol')
+print(f'jumlah telur {jumlah_telur} butir')
 
+if jumlah_botol_susu > 0:
+    print('Anak mengecek harganya, dan ternyata cukup uangnya')
+    if jumlah_telur >= 6:
+        print('Anak membeli 6 butir telur')
+    else:
+        print('Anak tidak membeli telur')
+    print('Anak membeli 1 botol susu')
+else:
+    print('Anak tidak jadi membeli susu')
+
+print('Anak pulang kerumah')
+print('Anak menyampaikan hasilnya kepada ibu')
