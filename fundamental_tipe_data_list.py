d8888d4586c68d75f5f411bedf88ef6090e1af07
rizaagusdev <riza.agus.a@gmail.com>
2023-11-29 22:49:54 +0700

    Type data list part 1

--- fundamental_tipe_data_list.py
+++ fundamental_tipe_data_list.py
@@ -26,3 +26,30 @@ daftar_buku.append('Dunia Matematika Kelas 5')
 for i in range(0, len(daftar_buku)):
     print(daftar_buku[i])
 #add comment
+
+print('\nClear list')
+daftar_buku.clear()
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
+
+print('\nGanti elemen pertama')
+daftar_buku = ['Seven Habits', 'How to Influence People', 'First Thing First', '4DX']
+daftar_buku[0] = 'Eight Habits'
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
+
+print('\nAmbil elemen kedua')
+buku = daftar_buku.pop(1)
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
+
+print('\nBuku yang diambil tadi')
+print(buku)
+
+print('\nKembalikan semua daftar buku kemudian pop')
+daftar_buku = ['Seven Habits', 'How to Influence People', 'First Thing First', '4DX']
+daftar_buku.pop(-4)
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
+
+
