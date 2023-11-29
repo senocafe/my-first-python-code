47ef52c98decda650cfd395a6bf4a1143b989215
rizaagusdev <riza.agus.a@gmail.com>
2023-11-29 22:50:44 +0700

    Dictionary

---
+++ fundamental_dict.py
@@ -0,0 +1,12 @@
+users = {
+    'id': 1,
+    'name': 'Leanne Graham',
+    'username': 'Bret',
+    'email': 'Sincere@april.biz',
+}
+
+print(users)
+print(users['id'])
+print(users['name'])
+print(users['username'])
+print(users['email'])
