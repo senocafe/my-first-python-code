5a1f540a5a77a76b5ee86229aa01efa04b0b065f
rizaagusdev <riza.agus.a@gmail.com>
2023-11-30 16:08:39 +0700

    Dict dalam dict atau json dalam json

--- fundamental_dict.py
+++ fundamental_dict.py
@@ -3,6 +3,16 @@ users = {
     'name': 'Leanne Graham',
     'username': 'Bret',
     'email': 'Sincere@april.biz',
+    'address': {
+        'street': 'Kulas Light',
+        'suite': 'Apt. 556',
+        'city': 'Gwenborough',
+        'zipcode': '92998-3874',
+        'geo': {
+            'lat': '-37.3159',
+            'lng': '81.1496',
+        }
+    }
 }
 
 print(users)
@@ -10,3 +20,6 @@ print(users['id'])
 print(users['name'])
 print(users['username'])
 print(users['email'])
+print(users['address']['street'])
+print(users['address']['geo']['lat'])
+print(users['address']['geo']['lng'])
